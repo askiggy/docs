06563ff54d0c9eb5d2ffbec1d33300b3586ff2ed
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-11-16 16:07:23 -0800

    Cleaning up rollup package script to exclude unused files

--- rollup.config.js
+++ rollup.config.js
@@ -4,20 +4,11 @@ import pkg from './package.json';
 
 const config = {
   input: pkg.source,
-  output: [
-    { file: pkg.module, format: 'esm' }
-  ],
   plugins: [
     del({ targets: ['dist/*'] }),
     copy({
       targets: [
         { src: pkg.files, dest: 'dist' },
-        {
-          src: 'package.json',
-          dest: 'dist',
-          transform: (contents) => contents.toString()
-            .replace(/dist\//g, '')
-        }
       ]
     })
   ]
